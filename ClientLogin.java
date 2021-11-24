90029c7d75fe093e8aba3e1c4795d4d1fd2762f4
Anthony-Lagreca <94651568+Anthony-Lagreca@users.noreply.github.com>
2021-11-23 20:34:16 -0500

    Add files via upload
    Here's the base GUI I made for the project, it's not super detailed so let me know what else I should do / add

---
+++ ClientLogin.java
@@ -0,0 +1,121 @@
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ * ClientLogin - Allows clients to login to connect with other members
+ * Name: Anthony, Justin, Isaac, Charan
+ * Course/Section: ISTE-121-02
+ */
+
+public class ClientLogin extends Application implements EventHandler<ActionEvent> {
+   // Window objects
+   Stage stage = null;
+   Scene scene = null;
+   VBox root = null;
+   
+   // GUI Components
+   private TextField tfIPAddress = new TextField();
+   private TextField tfPort = new TextField();
+   private TextField tfName = new TextField();
+   private TextField tfPass = new TextField();
+   private String strPort = null;
+   private String strIP = null;
+   private String strName = null;
+   private Button btnConnect = new Button("Connect");
+   private Button btnGetNames = new Button("Send Message");
+   private TextArea taMessages = new TextArea();
+   
+   // Networking
+   private Socket socket = null;
+   private static final int SERVER_PORT = 35672;
+   private PrintWriter pwt = null;
+   private Scanner scn = null;
+     
+   /**
+    * Main program ... 
+    * @args - command line arguments (ignored)
+    */
+   public static void main(String[] args) {
+      launch(args);
+   }
+   
+   /** constructor */
+   public void start(Stage _stage) {
+      // Window set up
+      stage = _stage;
+      stage.setTitle("Client Login");
+      final int WIDTH = 500;
+      final int HEIGHT = 500;
+      final int X = 50;
+      final int Y = 100;
+      stage.setX(X);
+      stage.setY(Y);
+      stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+         public void handle(WindowEvent evt) { System.exit(0); }
+      });
+      
+      // Draw the GUI
+      root = new VBox();
+      
+      HBox hbTop = new HBox(10);
+      hbTop.getChildren().addAll(new Label("IP Address"), tfIPAddress, new Label("Port"), tfPort, btnConnect);
+      
+      HBox hbBot = new HBox();
+      hbBot.setAlignment(Pos.CENTER);
+      hbBot.getChildren().addAll(btnGetNames);
+      
+      root.getChildren().addAll(hbTop, taMessages, hbBot);
+      for(Node n: root.getChildren()) {
+         VBox.setMargin(n, new Insets(10));
+      }
+      
+      // Handle button clicks
+      btnConnect.setOnAction(this);
+      btnGetNames.setOnAction(this);
+      
+      // Set the scene and show the stage
+      scene = new Scene(root, WIDTH, HEIGHT);
+      stage.setScene(scene);
+      stage.show();   
+      
+      // Adjust settings
+      taMessages.setPrefHeight(HEIGHT - hbTop.getPrefHeight() - hbBot.getPrefHeight());
+      tfIPAddress.setPrefColumnCount(12);
+      tfPort.setPrefColumnCount(7); 
+      taMessages.setWrapText(true);
+   }
+   
+   /** Button handler */
+   public void handle(ActionEvent ae) {
+      switch(((Button)ae.getSource()).getText()) {
+         case "Send Message":
+            //doSendMessage();
+            break;
+         case "Connect":
+            //doConnect();
+            break;
+         case "Disconnect":
+            //doDisconnect();
+            break;
+      }
+   }
+   
+//    public void doSendMessage() {
+//    }
+//    
+//    public void doConnect() {
+//    }
+//    
+//    public void doDisconnect() {
+//    }
+   
+}
